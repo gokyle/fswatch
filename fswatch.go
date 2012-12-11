64aba55cd6850559f28e32ea19d645b2597263f1
Kyle Isom <coder@kyleisom.net>
2012-12-10 17:31:48 -0700

    start rearchitected version of fswatch

--- fswatch.go
+++ fswatch.go
@@ -6,18 +6,83 @@ import (
 )
 
 const (
-	CREATED = iota
+	NONE = iota
+	CREATED
 	DELETED
 	MODIFIED
+	NOEXIST
 	NOPERM
 	INVALID
 )
 
+var watch_delay time.Duration
+
+func init() {
+        del, err := time.ParseDuration("100ms")
+        if err != nil {
+                panic("couldn't set up fswatch: " + err.Error())
+        }
+        watch_delay = del
+}
 type watchItem struct {
 	Path      string
-	last_mod  time.Time
-	Exists    bool
+	StatInfo  os.FileInfo
 	LastEvent int
+}
+
+func watchPath(path string) (wi *watchItem) {
+	wi = new(watchItem)
+	wi.Path = path
+	wi.LastEvent = NONE
+
+	fi, err := os.Stat(path)
+	if err == nil {
+		wi.StatInfo = fi
+	} else if os.IsNotExist(err) {
+		wi.LastEvent = NOEXIST
+	} else if os.IsPermission(err) {
+		wi.LastEvent = NOPERM
+	} else {
+		wi.LastEvent = INVALID
+	}
+	return
+}
+
+func (wi *watchItem) Update() bool {
+	fi, err := os.Stat(wi.Path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			if wi.LastEvent == NOEXIST {
+				return false
+			} else if wi.LastEvent == DELETED {
+				wi.LastEvent = NOEXIST
+				return false
+			} else {
+				wi.LastEvent = DELETED
+				return true
+			}
+		} else if os.IsPermission(err) {
+			if wi.LastEvent == NOPERM {
+				return false
+			} else {
+				wi.LastEvent = NOPERM
+				return true
+			}
+		} else {
+			wi.LastEvent = INVALID
+			return false
+		}
+	}
+
+	if fi.ModTime().After(wi.StatInfo.ModTime()) {
+		switch wi.LastEvent {
+		case NONE, CREATED, NOPERM, INVALID:
+			wi.LastEvent = MODIFIED
+		case DELETED, NOEXIST:
+			wi.LastEvent = CREATED
+		}
+	}
+	return true
 }
 
 type Notification struct {
@@ -25,50 +90,34 @@ type Notification struct {
 	Event int
 }
 
-func getLastMod(path string) (mt time.Time, err error) {
-	fi, err := os.Stat(path)
-	if err != nil {
-		return
-	}
-	return fi.ModTime(), nil
-}
-
-func (wi *watchItem) Update() (updated bool) {
-	mt, err := getLastMod(wi.Path)
-	switch {
-	case os.IsNotExist(err):
-		if wi.LastEvent != NOPERM {
-			wi.LastEvent = NOPERM
-			updated = true
-		}
-	case os.IsPermission(err):
-		if wi.LastEvent != NOPERM {
-			wi.LastEvent = NOPERM
-			updated = true
-		}
-	default:
-		if wi.LastEvent != INVALID {
-			wi.LastEvent = INVALID
-			updated = true
-		}
-	}
-	if updated {
-		return
-	}
-
-	if mt != wi.last_mod {
-		wi.last_mod = mt
-		updated = true
-	}
-	return
-}
-
 func (wi *watchItem) Notification() *Notification {
 	return &Notification{wi.Path, wi.LastEvent}
 }
 
 type Watcher struct {
-	items  map[string]*watchItem
-	notify chan *Notification
-	delay  time.Duration
+	paths       map[string]*watchItem
+	notify_chan chan *Notification
+        auto_watch  bool
+}
+
+func newWatcher(dir_notify bool, paths ...string) (w *Watcher) {
+        if len(paths) == 0 {
+                return
+        }
+        w = new(Watcher)
+        w.auto_watch = dir_notify
+        w.paths = make(map[string]*watchItem, 0)
+
+        for _, path := range paths {
+                w.paths[path] = watchPath(path)
+        }
+        return
+}
+
+func Watch(paths ...string) *Watcher {
+        return newWatcher(false, paths...)
+}
+
+func WatchDir(paths ...string) *Watcher {
+        return newWatcher(true, paths...)
 }
